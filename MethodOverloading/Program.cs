9afa8e575879645200d6858bb1b334e1a470cff4
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-05-18 14:20:55 -0400

    What a day with these Methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Addmethod(0, 1, true);
         }
+
+        public static int Addmethod(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Addmethod(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Addmethod(int num3,int num4, bool sam)
+        {
+            if (sam == true && num3 + num4 == 1)
+            {
+                return $"{num3 + num4} dollar";
+            }
+
+            else if (sam == true && num3 + num4 != 1)
+            {
+                return $"{num3 + num4} dollars";
+            }
+            else
+            {
+                return $"{num3 + num4}";
+            }
+        }
+            
+            
+        
+        
+        
+
+        
+
     }
+
 }
